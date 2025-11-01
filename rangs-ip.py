cc43a8b4de086fa8c09b501aaea49e1700fd014f
RangS <rangga19sj@gmail.com>
2025-11-01 20:12:56 +0700

    Update rangs-ip.py

--- rangs-ip.py
+++ rangs-ip.py
@@ -11,6 +11,10 @@ print(Fore.RED + """
 
 host = input(Fore.BLUE + "\n Enter a domain name: ")
 ip = socket.gethostbyname(host)
+print(" ======================\n Geolocation:")
+if not host:
+        print(Fore.RED + "\n==================\nBruh -_-' Enter a domain Bro!")
+        print(Fore.RED + "==================\n")
 
 request = 'https://geolocation-db.com/jsonp/' + ip
 response = requests.get(request)
@@ -19,6 +23,7 @@ geolocation = geolocation.split("(")[1].strip(")")
 geolocation = json.loads(geolocation)
 
 for k,v in geolocation.items():
+    
     pprint.pprint(str(k) + ' : ' + str(v))
 
 print(Fore.GREEN + f"\nSo, the IP Address of the website is {ip}")
